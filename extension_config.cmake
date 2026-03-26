a6a1146a79d3cfc8089198f5178c0e749e445a07
Tomáš Linhart <pasmen@gmail.com>
2026-03-26 16:52:50 +0100

    Add tests

--- extension_config.cmake
+++ extension_config.cmake
@@ -6,3 +6,10 @@ duckdb_extension_load(http_stats
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
 )
+
+# HTTPFS extension and its dependencies (needed for tests that make HTTP requests).
+duckdb_extension_load(json)
+duckdb_extension_load(parquet)
+duckdb_extension_load(httpfs
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/duckdb-httpfs
+)
